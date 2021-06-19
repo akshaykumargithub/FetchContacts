ba0cd9ab13aae2df92e18da14ac3a378237e6242
magenative_akshay <akshaykumar@magenative.com>
2021-06-19 13:15:21 +0530

    Minor item changes

--- app/src/main/java/fetch/contacts/model/Contact.kt
+++ app/src/main/java/fetch/contacts/model/Contact.kt
@@ -1,7 +1,7 @@
 package fetch.contacts.model
 
 class Contact {
-    var contactName: String? = null
+    var     contactName: String? = null
     var contactNumber: String? = null
 
     constructor(contactMame: String?, contactNumber: String?) {
